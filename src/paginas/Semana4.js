f39709f18106d2da6d7a51d95403fc3cdf396950
CaioNishijo <110948758+CaioNishijo@users.noreply.github.com>
2025-05-26 20:02:18 -0300

    Feito parte de signup

--- src/paginas/Semana4.js
+++ src/paginas/Semana4.js
@@ -1,8 +1,9 @@
 import Toolbar from "../botoes/Toolbar"
+import SignUp from "./SignUp"
 
 export default function Semana4(){
     return <>
         <Toolbar></Toolbar>
-    
+        <SignUp></SignUp>
     </>
 }
--- src/paginas/SignUp.js
+++ src/paginas/SignUp.js
@@ -1,1 +1,9 @@
-expo
+export default function SignUp(){
+    return <form onSubmit={(e) => {
+            e.preventDefault()
+            alert('Enviando!')
+        }}>
+        <input></input>
+        <button>Enviar</button>
+    </form>
+}
